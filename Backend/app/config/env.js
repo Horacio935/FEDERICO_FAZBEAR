e7066cd73fd4b7803dd03ec27b2f14adf1ea726a
Horacio935 <lopezjosecbsl@gmail.com>
2024-10-20 16:26:06 -0600

    STRIPE3

--- Backend/app/config/env.js
+++ Backend/app/config/env.js
@@ -4,7 +4,6 @@ module.exports = {
   DB_HOST: 'adb.mx-queretaro-1.oraclecloud.com',  // Cambia Pandnaceous por 127.0.0.1 para asegurar la conexión local
   DB_PORT: '1521',
   DB_NAME: 'g18bd7700c4df3e_fazbear_low.adb.oraclecloud.com', // Cambia FAZBEAR_DB por xe, que es el servicio que está registrado
-  STRIPE_SECRET_KEY: sk_test_51Q9HPsB93apspbx9AA2EX09UQmgIOAihaA7IGZ4I4EyZh9ya4UqouiaiYTJLtpBLmFq0XdSed99PW8dbIdPwRChd00OQqPtAZ1,
   pool: {
     max: 5,
     min: 0,
